03b07247a90ffc1baa52b5474f897bf4eb77013c
DanielSemu <danielsemu06@gmail.com>
2024-01-19 11:55:28 +0300

    19/1/2024 noon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 
 import './App.css';
 import Homepage from './pages/homepage/homepage.components';
+import { Route, Routes } from 'react-router-dom';
+const HatsPage =()=>(
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+
+)
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+     <Routes>
+        <Route exact path='/' Component={Homepage}/>
+        <Route exact path='/hats' Component={HatsPage}/>
+     </Routes>
     </div>
   );
 }
--- src/pages/homepage/homepage.components.js
+++ src/pages/homepage/homepage.components.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './homepage.style.scss'
 import Directory from '../../components/directory/directory.component'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
   return (
     <div className='homepage'>
+      <Link to={'/hats'}> <h1>Hats</h1></Link>
         <Directory/>
     </div>
   )
